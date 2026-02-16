3fd1ebcadd7d977d7ab0f1a9f8716abe8acfe26e
tushar <tusharbansal3260@gmail.com>
2026-02-16 12:32:44 +0530

    tushar

--- src/main/java/org/example/todoapp2/repository/repository.java
+++ src/main/java/org/example/todoapp2/repository/repository.java
@@ -1,4 +1,7 @@
 package org.example.todoapp2.repository;
 
-public interface repository {
+import org.example.todoapp2.model.model;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface repository extends MongoRepository<model ,String> {
 }
--- src/main/java/org/example/todoapp2/service/service.java
+++ src/main/java/org/example/todoapp2/service/service.java
@@ -1,4 +1,5 @@
 package org.example.todoapp2.service;
 
 public class service {
+
 }
